b2c2d8343943f966aecf451beaf5c2919250a692
Brandon Foster <Brandon.Foster81@myhunter.cuny.edu>
2019-04-18 13:54:07 -0400

    quick backup

--- website/app/controllers/notebooks_controller.rb
+++ website/app/controllers/notebooks_controller.rb
@@ -1,5 +1,7 @@
 class NotebooksController < ApplicationController
 	def user
+		@notebook = Notebook.create(name: "My notebook")
+
 		@userlogin = true
 		@navbars = Navbar::FULL_TOP | Navbar::FOOTER
 	end
--- website/app/models/notebook.rb
+++
@@ -1,6 +0,0 @@
-class Notebook
-  include Mongoid::Document
-
-  field :name,          type: String, default: ""
-	field :notes,					type: Array
-end
