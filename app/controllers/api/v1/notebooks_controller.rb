5947e67973fb39dd265e141b7ef2ea25d44b263e
Brandon Foster <Brandon.Foster81@myhunter.cuny.edu>
2019-05-15 21:55:28 -0400

    notebook index api call

--- app/controllers/api/v1/notebooks_controller.rb
+++ app/controllers/api/v1/notebooks_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::NotebooksController < Api::ApiBaseController
 	before_action :authenticate_user!
 	
 	def index
-		render json: Notebook.first
+		# Public Notebooks Viewing
+		render json: Notebook.any_of({ private: false })
 	end
 
 	def user
@@ -12,7 +13,7 @@ class Api::V1::NotebooksController < Api::ApiBaseController
 			render json: @user.notebook_models(current_user.id)
 		rescue => ex
 			# 404 Error if user_id is not a registered user
-			render :file => "#{Rails.root}/public/404", :status => :not_found
+			render json: "user_id is incorrect", :status => :not_found
 		end
 	end
 end
